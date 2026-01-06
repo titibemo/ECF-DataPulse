4edc9ee6e3dea15b5477ef65cfd7c764b79a8acd
Thierry <145456023+titibemo@users.noreply.github.com>
2026-01-06 17:04:11 +0100

    feat(analyses.sql): add queries 3 and 5 - windows function and crossing two differents datas

--- sql/analyses.sql
+++ sql/analyses.sql
@@ -14,7 +14,12 @@ JOIN geocoding geo
     ON pl.city = geo.city;
     
 /* 3. Une requête avec fonction de fenêtrage (window function) */
-
+SELECT
+    name_library,
+    city,
+    ca_by_year,
+    RANK() OVER (PARTITION BY city ORDER BY ca_by_year DESC) AS rank_ca_by_year
+FROM partner_library;
 
 /* 4. Une requête de classement (top N) */
 SELECT 
@@ -26,3 +31,12 @@ ORDER BY ca_by_year DESC
 LIMIT 2;
 
 /* 5. Une requête croisant au moins 2 sources de données */
+SELECT
+    pl.name_library,
+    pl.city,
+    pl.ca_by_year,
+    geo.postcode,
+    geo.citycode
+FROM partner_library pl
+JOIN geocoding geo
+    ON pl.city = geo.city
